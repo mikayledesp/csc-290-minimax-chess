66a24a1d6c7abe93e85d795f3de1cb739a8cca8b
Lucy Kreipke <lkreipke@smith.edu>
2025-10-20 18:16:35 -0400

    milestone 1 ??

--- minimaxChess.py
+++ minimaxChess.py
@@ -1,8 +1,28 @@
 import chess
 import random
 import datetime
+import math
 
-def main ():
+def getEval(captured_piece:chess.Piece|None) -> int:
+
+    if (captured_piece == None):
+        return 0
+    else:
+        if captured_piece.piece_type == chess.PAWN:
+            return 1
+        elif captured_piece.piece_type == chess.KNIGHT:
+            return 3
+        elif captured_piece.piece_type == chess.BISHOP:
+            return 3
+        elif captured_piece.piece_type == chess.ROOK:
+            return 5
+        elif captured_piece.piece_type == chess.QUEEN:
+            return 9
+        else:
+            return 0
+
+
+def main () -> None:
 
     print("="*33)
     print(f"\tWelcome to Chess!")
@@ -39,11 +59,31 @@ def main ():
             randIndex = random.randint(0,len(moveList)-1)
 
             botMove = moveList[randIndex]
-
+            best_score = -math.inf
+            best_move = None
             for move in moveList:
-                if board.is_capture(move):
-                    botMove = move
-                    break
+                baseBoard = chess.BaseBoard(board.board_fen())
+                move_piece = baseBoard.piece_at(move.to_square)
+                print(move_piece)
+                player_move_score = getEval(move_piece)
+                
+                board.push(move) 
+                moveList_opp = list(board.legal_moves)
+                
+                for opp_move in moveList_opp:
+                    baseBoard = chess.BaseBoard(board.board_fen())
+                    move_piece = baseBoard.piece_at(opp_move.to_square)
+                    print(move_piece)
+                    opp_move_score = getEval(move_piece)
+
+                    score = player_move_score - opp_move_score
+                    board.pop()
+
+                    if score > best_score:
+                        best_score = score
+                        best_move = move
+
+
                     
             print(f"{botName}: {botMove}")
 
